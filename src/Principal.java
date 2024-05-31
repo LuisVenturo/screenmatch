4a3fbd7083d322ea6a8cab7a2b18d95802d1fa8f
Luis Venturo <venturomoralesluis@gmail.com>
2024-05-31 16:25:07 -0500

    Programacion Orientada a Objetos

---
+++ src/Principal.java
@@ -0,0 +1,57 @@
+import com.luisventuro.screenmatch.calculos.CalculadoraDeTiempo;
+import com.luisventuro.screenmatch.calculos.FiltroRecomendacion;
+import com.luisventuro.screenmatch.modelos.Episodio;
+import com.luisventuro.screenmatch.modelos.Pelicula;
+import com.luisventuro.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragón");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
+
+        Pelicula otrapelicula = new Pelicula();
+        otrapelicula.setNombre("Matrix");
+        otrapelicula.setFechaDeLanzamiento(1998);
+        otrapelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otrapelicula);
+        System.out.println("Tiempo necesario para ver tus titulos favoritos estas vacaciones: "
+                +calculadora.getTiempoTotal() + " minutos");
+
+
+        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+        filtroRecomendacion.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setNombre("La casa Targaryen");
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(300);
+        
+         filtroRecomendacion.filtra(episodio);
+
+    }
+}
---
+++ src/com/luisventuro/screenmatch/calculos/Clasificacion.java
@@ -0,0 +1,6 @@
+package com.luisventuro.screenmatch.calculos;
+
+public interface Clasificacion {
+
+    int getClasificacion();
+}
